9a34eda64ab347b939078e75d2787742a730f221
simulot <jfc@responsiveconsulting.fr>
2017-01-28 19:30:44 +0100

    add parameter mask

--- cmd/gogrep.go
+++ cmd/gogrep.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/pkg/profile"
 	"github.com/simulot/gogrep/gogrep"
 	// Register common walkers
 	_ "github.com/simulot/golib/file/walker/zipwalker"
 )
 
 func main() {
-	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	app := &gogrep.AppSettings{}
 	err := app.Commandline()
 	if err != nil {
